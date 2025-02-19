3e79cd97db96bc31339de1a44e6f5779d0c4476a
knownocode <33673602+knownocode@users.noreply.github.com>
2025-02-19 15:54:48 +0000

    docs: add comments

--- sort.py
+++ sort.py
@@ -3,7 +3,7 @@ def bubble_sort(arr):
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1, arr[j]]
+                arr[j], arr[j+1] = arr[j+1, arr[j]] # This is good
     return arr
 
 def insertion_sort(arr):
@@ -11,7 +11,7 @@ def insertion_sort(arr):
         key = arr[i]
         j = i-1
         while j >= 0 and key < arr[j]:
-            arr[j+1] = arr[j]
+            arr[j+1] = arr[j] # This is bad
             j -= 1
         arr[j+1] = key
     return arr
@@ -33,7 +33,7 @@ def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
+    left = [x for x in arr if x < pivot] # This is good
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
     return quick_sort(left) + middle + quick_sort(right)
